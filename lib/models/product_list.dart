f2a9e6a12f02b96615893f7313fc6f5a71b73f5b
Aguiar <beca.cout@gmail.com>
2023-01-31 15:13:18 -0300

    using provider and changeNotifier

---
+++ lib/models/product_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:loja/data/dummy_data.dart';
+
+import 'product.dart';
+
+//usando mixin
+class ProductList with ChangeNotifier {
+  final List<Product> _items = dummyProducts;
+
+  //Disponibilizando um clone da lista de produtos por meio de um get
+  List<Product> get items => [..._items];
+
+  //Método de adicionar produto na lista
+
+  void addProduct(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:loja/models/product.dart';
+import 'package:loja/models/product_list.dart';
+import 'package:provider/provider.dart';
 
 import '../components/product_item.dart';
 import '../data/dummy_data.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
     return Scaffold(
         appBar: AppBar(
           title: Text("Minha Loja"),
